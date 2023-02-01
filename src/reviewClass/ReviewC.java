79747927d7fa38ab1584f97b144622ded2e0ec11
raqeebtek <122577342+raqeebtek@users.noreply.github.com>
2023-01-31 20:55:33 -0800

    Update ReviewC.java
    Main method and review method removed

--- src/reviewClass/ReviewC.java
+++ src/reviewClass/ReviewC.java
@@ -2,14 +2,6 @@ package reviewClass;
 
 public class ReviewC {
 
-	public static void main(String[] args) {
-
-
-	}
 	
-	public void test() {
-		System.out.println("This mehtod is on Review Session Branch");
-		
-	}
 
 }
